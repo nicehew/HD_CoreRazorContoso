b66b3facf00b91e40953839666ef7dda9311b22b
Hew Dias <21616076@bucks.ac.uk>
2019-10-10 11:41:04 +0100

    Add project files.

---
+++ TH2_CoreRazerContoso/Pages/Converter.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TH2_CoreRazerContoso.Pages
+{
+    public class ConverterModel : PageModel
+    {
+        public decimal Pounds { get; set; }
+        public decimal Euros { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
---
+++ TH2_CoreRazerContoso/Pages/Hello.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TH2_CoreRazerContoso.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public string UserName { get; set; }
+        public void OnGet()
+        {
+            UserName = null;
+        }
+
+        public void OnPost()
+        {
+            this.UserName = Request.Form["UserName"];
+        }
+    }
+}
